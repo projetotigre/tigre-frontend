d5c32fc59f9c3982a2096dda5fc3b53ec7c0271d
Gabriel Vitor <gvsrepins@gmail.com>
2014-05-27 04:23:28 -0300

    show areas de atuacao filter

--- js/main.js
+++ js/main.js
@@ -7,20 +7,23 @@ $('document').ready(function(){
 			$(this).css("right", "200px");
 			$('.config-box').css("right", "0");
 			$(this).addClass("open", {duration:1000});
+
 		} else {
+
 			$(this).css("right", "0");
 			$('.config-box').css("right", "-200px");
 			$(this).removeClass("open", {duration:1000})
 		}
 	});	
 
+	//faz uma requisicação ajax e exibe os dados de acordo com um template
 	$.getJSON('http://localhost/tigre/frontend/data/areas_atuacao', function(data){
 
-		$.each(function(){
-			$('#areas-atuacao .checkbox').each({
-				
-			});
-		})
+		var data =  { areas_atuacao:data}; //seta os dados na key areas_atuacao
+		var template = $('#areas-atuacao-tpl').html(); //pega o template		
+		var html = Mustache.to_html(template, data); //insere as variaveis no template
+		
+		$('#areas-atuacao').html(html); //exibe na div com #areas-atuacao
 		
 	});
 });
