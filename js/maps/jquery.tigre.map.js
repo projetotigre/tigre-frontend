d2ca3e8c050c0b1cec5a75de6f0f09e51e0860cc
Luiz Blanes <blanes@blanes.com.br>
2014-05-30 22:59:00 -0300

    Adicionado cores nos marcadores

--- js/maps/jquery.tigre.map.js
+++ js/maps/jquery.tigre.map.js
@@ -11,6 +11,9 @@ function convertAddressToLatLng(address, geocode_callback)
 
 			geocode_callback(lat_lng);
 
+//	        } else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {    
+//			setTimeout(2000);
+
 		} else {
 			console.log('Geocode was not successful for the following reason: ' + status);
 		}
@@ -47,6 +50,8 @@ $('document').ready(function()
 		var markers = [];
 		var circles = [];
 		var ano, natureza_juridica;
+		var circle_color;
+		var mark_icon;
 
 		function clearMap(){
 			//apaga os markers		
@@ -78,52 +83,120 @@ $('document').ready(function()
 	        		var html = Mustache.to_html(template, {convenios:data.organizacoes}); //insere as variaveis no template
 	        		$('#table-convenios').append(html); 
 
+				$.each(data.organizacoes, function(index, ponto){
 
-					$.each(data.organizacoes, function(index, ponto){
+					var address = ponto.endereco + ', ' + ponto.cidade + ', ' + ponto.estado;
 
-						var address = ponto.endereco + ', ' + ponto.cidade + ', ' + ponto.estado;
+					switch (ano){
+//						case "Entidade Privada Sem Fins Lucrativos":				
+						case "2014":				
+						circle_color="#3FA7D8"
+						break;			
+						case "2013":				
+						circle_color="#F00000";
+						break;											
+						case "2012":				
+						circle_color="#AFA000"
+						break;											
+						case "2011":				
+						circle_color="#0AFA00"
+						break;											
+						case "2010":				
+						circle_color="#00AFA0"
+						break;			
+						case "2009":				
+						circle_color="#000AFA";
+						break;											
+						case "2008":				
+						circle_color="#0000AF"
+						break;											
+						case "2007":				
+						circle_color="#AAAAAA"
+						break;											
+					}
 
-						convertAddressToLatLng(address, function( latLng ){
+					convertAddressToLatLng(address, function( latLng ){
 
-							convenioOptions = {
-						  		strokeColor: '#0000ff',
-						  		strokeOpacity: 0.8,
-						  		strokeWeight: 2,
-						  		fillColor: '#0000ff',
-						  		fillOpacity: 0.35,
-						  		map: map,
-						  		center: latLng,
-						  		radius: Math.sqrt(ponto.valor_repasse_uniao) * 80
-						  	};
+						convenioOptions = {
+					  		strokeColor: circle_color,
+					  		strokeOpacity: 0.8,
+					  		strokeWeight: 2,
+					  		fillColor: circle_color,
+					  		fillOpacity: 0.35,
+					  		map: map,
+					  		center: latLng,
+					  		radius: Math.sqrt(ponto.valor_repasse_uniao) * 80
+						};
 
-						    // Add the circle for this city to the map.
-						    cityCircle = new google.maps.Circle(convenioOptions);
+						// Add the circle for this city to the map.
+						cityCircle = new google.maps.Circle(convenioOptions);
 
-							//intanciando marcadores
-				            var marker = new google.maps.Marker({
-				              position: latLng,
-				              title: ponto.nome,
-				              map: map
-				            });
+						//intanciando marcadores
+//						var marker = new google.maps.Marker({
+//							position: latLng,
+//							title: ponto.nome,
+//							map: map
+//		            			});
 
-
-							google.maps.event.addListener(marker, 'click', function() {
-						          infowindow.setContent('R$' + ponto.valor_repasse_uniao.format(2, 3, '.', ',') +' - ' +ponto.nome);
-						          infowindow.open(map, marker);
-						    });
-
-							markers.push(marker);
-							circles.push(cityCircle);
+					switch (natureza_juridica){
+						case "Entidade Privada Sem Fins Lucrativos":				
+						mark_icon="img/markers/flag_blue_48.png"
+						break;			
+						case "2013":				
+						mark_icon="img/markers/flag_red_48.png";
+						break;											
+						case "2012":				
+						mark_icon="img/markers/flag_green_48.png"
+						break;											
+						case "2011":				
+						mark_icon="img/markers/flag_blue_48.png"
+						break;											
+						case "2010":				
+						mark_icon="img/markers/flag_blue_48.png"
+						break;			
+						case "2009":				
+						mark_icon="img/markers/flag_blue_48.png";
+						break;											
+						case "2008":				
+						mark_icon="img/markers/flag_blue_48.png"
+						break;											
+						case "2007":				
+						mark_icon="img/markers/flag_blue_48.png"
+						break;											
+					}
+						
+						var marker = new MarkerWithLabel({
+							position: latLng,
+							draggable: false,
+							raiseOnDrag: false,
+							map: map,
+							icon: mark_icon,
+//							icon: 'img/markers/flag_blue_48.png',
+//							labelContent: 'R$' + ponto.valor_repasse_uniao,
+							labelContent: natureza_juridica,
+							labelAnchor: new google.maps.Point(22, 0),
+							labelClass: "labels", // the CSS class for the label
+							labelStyle: {opacity: 0.75}
 						});
 
-	        		});//endforeach
+						google.maps.event.addListener(marker, 'click', function() {
+							infowindow.setContent('R$' + ponto.valor_repasse_uniao.format(2, 3, '.', ',') +' - ' +ponto.nome);
+							infowindow.open(map, marker);
+						});
 
-					markerCluster = new MarkerClusterer(map, markers);
-				}//end if lenght
+						markers.push(marker);
+						circles.push(cityCircle);
+						markerCluster = new MarkerClusterer(map, markers);
 
-			}); 
+					});
+
+		        	});//endforeach
+
+			}//end if lenght
+
+		}); 
 			
-		}
+	}
 
 		carregaDados();
 
